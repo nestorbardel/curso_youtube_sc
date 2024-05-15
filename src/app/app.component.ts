cd582cd0f37e265be788c3256732e74ce8039c34
Nestor <nestorbardel@gmail.com>
2024-05-15 10:15:43 -0300

    first commit curso componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'curso-angular-youtube';
+  title = ' NESTOR';
+  subtitle = ' es la persona que lo usa';
 }
